9bc7fcc45fda7924b6210e6ac7109a78c83677ce
Asilbek Asilov <asil.california@gmail.com>
2023-01-03 02:19:40 -0800

    Add func to play a single round

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
-plays = ['rock', 'paper', 'scissors'];
+plays = ['rock', 'paper', 'scissors']
 
 // get computers choice
-const getComputerChoice = () => {
-   const randomNumber = Math.floor(Math.random() * 3)
-   return plays[randomNumber]
+function getComputerChoice() {
+	const randomNumber = Math.floor(Math.random() * 3)
+	return plays[randomNumber]
 }
 
-console.log(getComputerChoice());
+const computerSelection = getComputerChoice()
+const playerSelection = 'paper'
+
+function playRound(playerSelection, computerSelection) {
+	// tie if both choices are same
+    // rock beats scissors
+    // scissors beat paper
+    // paper beats rock
+	if (playerSelection === computerSelection) {
+		return 'It is a Tie Game'
+	} else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+		return 'You win! Rock beats Scissors'
+	} else if (playerSelection === 'rock' && computerSelection === 'paper') {
+		return 'You lose! Paper beats Rock'
+	} else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+		return 'You win! Scissors beat paper'
+	} else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return 'You lose! Rock beats Scissors'
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+		return 'You win! Paper beats Rock'
+	} else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return 'You lose! Scissors beat Paper'
+    }
+}
+
+console.log(playRound(playerSelection, computerSelection))
